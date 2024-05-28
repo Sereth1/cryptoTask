b1bc207b6bc50292643b7900d269b3d7b0ce15be
Sereth1 <n1ghtw0olf@outlook.com>
2024-05-28 16:18:23 +0800

    ready

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
 "use client";
 import ExchangeRates from "@/components/ExchangeRates";
-import Image from "next/image";
-import KmdIcon from "cryptocurrency-icons/svg/color/AAVE.svg";
+
 import "./globals.css";
 export default function Home() {
   return (
     <div>
       <ExchangeRates />
-
-      <Image src={KmdIcon} width={50} height={50} />
     </div>
   );
 }
